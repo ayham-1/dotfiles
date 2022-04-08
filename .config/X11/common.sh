d82a7fa5fbdf2c4f42a91f34dbb76455653c8afd
ayham <altffour@protonmail.com>
2022-04-08 07:39:56 +0300

    switch to pipewire

---
+++ .config/X11/common.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+source ~/.config/env &
+
+/usr/bin/pipewire & /usr/bin/pipewire-pulse & /usr/bin/wireplumber &
+
+xset s 300 5 &
+xss-lock -n /usr/lib/xsecurelock/dimmer -l -- xsecurelock &
+keynav &
+light -S 100 &
+xset r rate 300 50 &
+umask 002 &
+keychain --absolute --dir "$XDG_RUNTIME_DIR"/keychain --eval --quiet id_ed25519
+unclutter --timeout 2 --start-hidden &
+xsetroot -xcf /usr/share/icons/WhiteSur-cursors/cursors/left_ptr 24 &
+xmodmap ~/.config/Xmodmap &
+toggle_touchpad &
+xhost +si:localuser:"$USER" &
+mpd &
