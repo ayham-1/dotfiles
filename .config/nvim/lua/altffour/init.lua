b57bd382ba863f2ddf17d01c64bba7b147260e6f
ayham <me@ayham.xyz>
2023-01-14 13:54:12 +0300

    update

--- .config/nvim/lua/altffour/init.lua
+++ .config/nvim/lua/altffour/init.lua
@@ -1,108 +1,63 @@
-vim.g.mapleader=" "
-vim.g.auto_reload_config = true;
-vim.g.gui_font_default_size = 13
-vim.g.gui_font_size = vim.g.gui_font_default_size
-vim.g.gui_font_face = "Monospace"
-
-RefreshGuiFont = function()
-  vim.opt.guifont = string.format("%s:h%s",vim.g.gui_font_face, vim.g.gui_font_size)
-end
-
-ResizeGuiFont = function(delta)
-  vim.g.gui_font_size = vim.g.gui_font_size + delta
-  RefreshGuiFont()
-end
-
-ResetGuiFont = function ()
-  vim.g.gui_font_size = vim.g.gui_font_default_size
-  RefreshGuiFont()
-end
-
--- Call function on startup to set default value
-ResetGuiFont()
-
--- Keymaps
-
-local opts = { noremap = true, silent = true }
-
-vim.keymap.set({'n', 'i'}, "<C-+>", function() ResizeGuiFont(1)  end, opts)
-vim.keymap.set({'n', 'i'}, "<C-->", function() ResizeGuiFont(-1) end, opts)
-vim.keymap.set({'n', 'i'}, "<C-BS>", function() ResetGuiFont() end, opts)
-
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 require("altffour.plugins")
 require("altffour.telescope")
 require("altffour.lsp")
-require("altffour.autocompletion")
 require("altffour.zk")
 require("altffour.cmp")
 require("altffour.snip")
 require("altffour.hydra")
+require("altffour.keymaps")
+require("altffour.neovide")
 
-P = function(v)
-  print(vim.inspect(v))
-  return v
-end
+-- general config
+vim.g.auto_reload_config = true
+vim.g.termguicolors = true
+vim.g.noswapfile = true
+vim.g.nobackup = true
+vim.g.nomodeline = true
+vim.g.nocompatible = true
+vim.g.noequalalways = true
+vim.g.wrap = true
+vim.g.textwidth = 80
+vim.g.wrapmargin = 80
+vim.g.ttimeoutlen = 0
+vim.g.autoread = true
+vim.g.backspace = indent, eol, start
+vim.g.ignorecase = true
+vim.g.smartcase = true
+vim.g.incsearch = true
+vim.g.magic = true
+vim.g.ruler = true
+vim.g.wildmenu = true
+vim.g.nosmd = true
+vim.g.hidden = true
+vim.g.nomodeline = true
+vim.g.undofile = true
+vim.g.tabstop = 4
+vim.g.softtabstop = 4
+vim.g.shiftwidth = 4
+vim.g.mouse = a
+vim.g.encoding = "utf-8"
+vim.g.backspace = indent, eol, start
+vim.opt.number = true
+vim.cmd("syntax on")
 
-_G.__luacache_config = {
-  chunks = {
-    enable = true,
-    path = vim.fn.stdpath('cache')..'/luacache_chunks',
-  },
-  modpaths = {
-    enable = true,
-    path = vim.fn.stdpath('cache')..'/luacache_modpaths',
-  }
-}
-require('impatient')
+-- appearance
+vim.number = true
+-- toggle invisible characters
+--set showbreak=↪
+--set list
+--set listchars=tab:→\ ,eol:¬,trail:⋅,extends:❯,precedes:❮,space:·
+--set listchars=tab:→\ ,eol:¬,trail:⋅,extends:❯,precedes:❮
+vim.ttyfast = true
 
-require('twilight').setup({
-		dimming = {
-				alpha = 0.25,
-				color = { "Normal", "#000000" },
-		}
-})
-require('zen-mode').setup({
-		width = .80,
-})
---require('lualine').setup({
---	options = {
---		theme = 'gruvbox'
---	}
---})
-require('diffview').setup()
---"vim.opt.termguicolors = true
-require('bufferline').setup()
-require('luasnip/loaders/from_vscode').load()
-
-require('Comment').setup()
-
-require("zen-mode").setup({
-  window = {
-    width = 80,
-    options = {
-      signcolumn = "no", -- disable signcolumn
-      number = false, -- disable number column
-      relativenumber = false, -- disable relative numbers
-      cursorline = false, -- disable cursorline
-      cursorcolumn = false, -- disable cursor column
-      foldcolumn = "0", -- disable fold column
-      list = false, -- disable whitespace characters
-    },
-  }
-})
-
-require("flutter-tools").setup{}
-require('Comment').setup()
-
--- nvim-lint
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require("lint").try_lint()
-  end,
-})
-
-
-require("typos").setup()
-
-require('neogit').setup {}
-require('gitsigns').setup ()
+-- Setup theme.
+vim.t_Co = 256
+--let g:gruvbox_termcolors=16
+--let g:gruvbox_contrast_light="hard"
+--let g:gruvbox_contrast_dark="hard"
+--colorscheme zellner
+--colorscheme wal
+--colorscheme 256_noir
+vim.cmd("colorscheme melange")
--- .config/nvim/lua/altffour/plugins.lua
+++ .config/nvim/lua/altffour/plugins.lua
@@ -1,64 +1,195 @@
---vim.cmd [[packadd packer.nvim]]
+-- Packer and Plugins setup
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
 
-return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-
-  use {
-    'goolord/alpha-nvim',
-    config = function ()
-      require('alpha').setup(require('alpha.themes.startify').config)
-    end
-  }
-  use {
-    "lewis6991/hover.nvim",
-    config = function()
-      require("hover").setup {
-	init = function()
-	  -- Require providers
-	  require("hover.providers.lsp")
-	  require('hover.providers.gh')
-	  require('hover.providers.gh_user')
-	  -- require('hover.providers.jira')
-	  require('hover.providers.man')
-	  require('hover.providers.dictionary')
-	end,
-	preview_opts = {
-	  border = nil
+require("lazy").setup({
+	-- common
+	"sindrets/diffview.nvim",
+	"kyazdani42/nvim-web-devicons",
+	"romgrk/barbar.nvim",
+	"L3MON4D3/LuaSnip",
+	"lewis6991/gitsigns.nvim",
+	"p00f/cphelper.nvim",
+	"numToStr/Comment.nvim",
+	"sbdchd/neoformat",
+	"jiangmiao/auto-pairs",
+	"andreasvc/vim-256noir",
+	"searleser97/cpbooster.vim",
+	"numToStr/Comment.nvim",
+	"ggandor/lightspeed.nvim",
+	"milkias17/reloader.nvim",
+	"simrat39/symbols-outline.nvim",
+	"jbyuki/venn.nvim",
+	"j-hui/fidget.nvim",
+	"sitiom/nvim-numbertoggle",
+	"nvim-lua/plenary.nvim",
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		dependences = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-fzy-native.nvim",
+		},
 	},
-	-- Whether the contents of a currently open hover window should be moved
-	-- to a :h preview-window when pressing the hover keymap.
-	preview_window = false,
-	title = true
-      }
+	{
+		"goolord/alpha-nvim",
+		config = function()
+			require("alpha").setup(require("alpha.themes.startify").config)
+		end,
+	},
+	{
+		"lewis6991/hover.nvim",
+		config = function()
+			require("hover").setup({
+				init = function()
+					-- Require providers
+					require("hover.providers.lsp")
+					require("hover.providers.gh")
+					require("hover.providers.gh_user")
+					-- require('hover.providers.jira')
+					require("hover.providers.man")
+					require("hover.providers.dictionary")
+				end,
+				preview_opts = {
+					border = nil,
+				},
+				-- Whether the contents of a currently open hover window should be moved
+				-- to a :h preview-window when pressing the hover keymap.
+				preview_window = false,
+				title = true,
+			})
 
-      -- Setup keymaps
-      vim.keymap.set("n", "K", require("hover").hover, {desc = "hover.nvim"})
-      vim.keymap.set("n", "gK", require("hover").hover_select, {desc = "hover.nvim (select)"})
-    end
-  }
+			-- Setup keymaps
+			vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
+			vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
+		end,
+	},
 
-  use 'anuvyklack/hydra.nvim'
-  use {
-    'lewis6991/gitsigns.nvim',
-    -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
-  }
-  
-  use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
+	"anuvyklack/hydra.nvim",
+	{
+		"lewis6991/gitsigns.nvim",
+		-- tag = 'release' -- To use the latest release (do not use this if you build Neovim nightly or dev builds!)
+	},
 
-  use {
-    "folke/which-key.nvim",
-    config = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-      require("which-key").setup {
-	-- your configuration comes here
-	-- or leave it empty to use the default settings
-	-- refer to the configuration section below
-      }
-    end
-  }
-  use 'simrat39/symbols-outline.nvim'
+	{ "TimUntersberger/neogit", dependences = "nvim-lua/plenary.nvim" },
 
-use 'lewis6991/impatient.nvim'
-end)
+	{
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 2000
+			require("which-key").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	},
 
+	-- colorschemes
+	"savq/melange",
+	"Lokaltog/vim-monotone",
+
+	-- lsp
+	"neovim/nvim-lspconfig",
+	"hrsh7th/nvim-compe",
+	"simrat39/symbols-outline.nvim",
+	"glepnir/lspsaga.nvim",
+	"williamboman/mason.nvim",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+
+	-- lint
+	"mfussenegger/nvim-lint",
+
+	-- nvim-cmp
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
+	"hrsh7th/nvim-cmp",
+
+	-- snippets
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+	"rafamadriz/friendly-snippets",
+
+	"nvim-telescope/telescope-fzy-native.nvim",
+
+	-- debug
+	"puremourning/vimspector",
+
+	-- writing
+	"preservim/vim-pencil",
+	"preservim/vim-markdown",
+	"poljar/typos.nvim",
+	"mickael-menu/zk-nvim",
+	"folke/twilight.nvim",
+	"folke/zen-mode.nvim",
+
+	-- coding language specific
+	"akinsho/flutter-tools.nvim",
+	"ziglang/zig.vim",
+})
+
+require("twilight").setup({
+	dimming = {
+		alpha = 0.25,
+		color = { "Normal", "#000000" },
+	},
+})
+require("zen-mode").setup({
+	width = 0.80,
+})
+require("diffview").setup()
+require("luasnip/loaders/from_vscode").load()
+
+require("Comment").setup()
+
+require("zen-mode").setup({
+	window = {
+		width = 80,
+		options = {
+			signcolumn = "no", -- disable signcolumn
+			number = false, -- disable number column
+			relativenumber = false, -- disable relative numbers
+			cursorline = false, -- disable cursorline
+			cursorcolumn = false, -- disable cursor column
+			foldcolumn = "0", -- disable fold column
+			list = false, -- disable whitespace characters
+		},
+	},
+})
+
+require("flutter-tools").setup({})
+require("Comment").setup()
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	callback = function()
+		require("lint").try_lint()
+	end,
+})
+
+require("typos").setup()
+
+require("neogit").setup({})
+require("gitsigns").setup()
+require("fidget").setup({})
+
+-- neoformat on save
+vim.cmd([[augroup fmt
+  autocmd!
+  autocmd BufWritePre * undojoin | Neoformat
+augroup END]])
