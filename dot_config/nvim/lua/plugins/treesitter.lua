26345243012e82870c6ce9c441542079a211595a
ayham-1 <me@ayham.xyz>
2025-01-09 10:13:16 +0100

    Update .config/nvim/lua/plugins/treesitter.lua

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ local M = {
 function M.config()
 	require("nvim-treesitter.configs").setup({
 		-- indentation
-		indent = { enable = true },
+		indent = { enable = false },
 		-- A list of parser names, or "all" (the four listed parsers should always be installed)
 		ensure_installed = { "cpp", "c", "lua", "vim" },
 
