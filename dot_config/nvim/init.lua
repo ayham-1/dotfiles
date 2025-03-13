51e2f04404940d4c4c56eb8a2f7592e2915451ab
ayham-1 <me@ayham.xyz>
2025-03-13 19:28:53 +0100

    revamp neovim config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,113 +1,167 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-vim.mapleader = " "
-vim.maplocalleader = " "
+vim.loader.enable()
 
--- Lazy.nvim and Plugins setup
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", {
-	defaults = { lazy = true },
-	checker = { enabled = true },
-	dev = { path = "~/.local/src/", fallback = false },
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"gzip",
-				"matchit",
-				"matchparen",
-				"netrwPlugin",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zipPlugin",
-			},
-		},
-	},
-})
+-- Mapleaders
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
-require("setups")
-require("keymaps")
-require("neovide")
-
--- general config
-vim.g.noswapfile = true
-vim.g.nobackup = true
-vim.g.nomodeline = true
+-- Configurations 
 vim.g.nocompatible = true
-vim.g.noequalalways = true
-vim.g.wrap = true
-vim.g.textwidth = 80
-vim.g.wrapmargin = 80
 vim.g.ttimeoutlen = 0
 vim.g.autoread = true
-vim.g.ignorecase = true
 vim.g.smartcase = true
 vim.g.incsearch = true
-vim.g.magic = true
-vim.g.ruler = true
 vim.g.nomodeline = true
-vim.g.undofile = true
-vim.g.mouse = a
+vim.g.ruler = true
+vim.g.mouse = "a"
 vim.g.encoding = "utf-8"
-vim.opt.conceallevel = 1
+vim.g.ttyfast = true
+
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.cmd("set scl=no")
-vim.g.NERDTreeChDirMode = 2
-vim.cmd("set clipboard+=unnamedplus")
 
 vim.cmd("syntax on")
-vim.opt_local.conceallevel = 2
+vim.cmd("set clipboard+=unnamedplus")
 
--- appearance
-vim.number = true
--- toggle invisible characters
---set showbreak=↪
---set list
---set listchars=tab:→\ ,eol:¬,trail:⋅,extends:❯,precedes:❮,space:·
---set listchars=tab:→\ ,eol:¬,trail:⋅,extends:❯,precedes:❮
-vim.ttyfast = true
-
--- Setup theme.
+-- Colorscheme
 vim.t_Co = 256
-vim.o.background = "dark"
---let g:gruvbox_termcolors=16
---let g:gruvbox_contrast_light="hard"
---let g:gruvbox_contrast_dark="hard"
---colorscheme zellner
---colorscheme wal
---colorscheme 256_noir
-vim.cmd("let g:gruvbox_contrast_light=\"hard\"")
-vim.cmd("let g:gruvbox_contrast_dark=\"hard\"")
-vim.cmd("colorscheme industry")
+vim.cmd.colorscheme("industry")
 
-vim.api.nvim_exec([[
-augroup fmt
-  autocmd!
-  autocmd BufWritePre * undojoin | Neoformat
-augroup END
-]], false)
+--- Swap, Backup, Undo Directories
+local homedir = vim.fn.expand("~/")
+vim.opt.directory = homedir .. "/.cache/nvim/swap"
+vim.opt.backupdir = homedir .. "/.cache/nvim/backup"
+vim.opt.undodir = homedir .. "/.cache/nvim/undo"
 
+-- Diagnostics
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+})
+vim.keymap.set('n', "<leader>do", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true, desc = "Diagnostic Float" })
+
+-- Setup lazy.nvim
+require("lazy").setup {
+  spec = {
+    { "dstein64/vim-startuptime", event = "VeryLazy" },
+
+    { "echasnovski/mini.nvim", version = "*" },
+    { "nvim-tree/nvim-web-devicons", opts = {} }, 
+
+    { "nvim-lua/plenary.nvim" },
+    { "BurntSushi/ripgrep" },
+    {  "nvim-treesitter/nvim-treesitter", build = function() require("nvim-treesitter.install").update({ with_sync = true })() end },
+    { "sbdchd/neoformat" },
+
+    { "nvim-telescope/telescope.nvim", tag = "0.1.8" },
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release" },
+
+    { "neovim/nvim-lspconfig" },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-vsnip" },
+    { "hrsh7th/vim-vsnip" },
+    { "onsails/lspkind.nvim" },
+
+    { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
+    { "hiphish/rainbow-delimiters.nvim" },
+
+    { "folke/which-key.nvim",  event = "VeryLazy", opts = { preset = "helix" } },
+  },
+  defaults = { lazy = false, },
+  install = { colorscheme = { "industry" } },
+  checker = { enabled = false },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "netrw", "netrwPlugin", "netrwSettings", "netrwFileHandlers", "gzip", "zip", "zipPlugin", "tar", "tarPlugin",
+        "getscript", "getscriptPlugin", "vimball", "vimballPlugin", "2html_plugin", "logipat", "rrhelper",
+        "spellfile_plugin", "matchit"
+      },
+    },
+  },
+}
+require("mini.icons").setup()
+require("rainbow-delimiters.setup").setup()
+
+-- Lsp/Mason
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+  function (server_name)
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    require("lspconfig")[server_name].setup { capabilities = capabilities }
+  end
+}
+
+-- Telescope
+require("telescope").setup()
+require("telescope").load_extension("fzf")
+local pickers = require("telescope.builtin")
+vim.keymap.set('n', "<leader>tf", pickers.find_files, { desc = "Telescope find files" })
+vim.keymap.set('n', "<leader>tg", pickers.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set('n', "<leader>tb", pickers.buffers, { desc = "Telescope buffers" })
+vim.keymap.set('n', "<leader>th", pickers.help_tags, { desc = "Telescope help tags" })
+vim.keymap.set('n', "<leader>tc", pickers.commands, { desc = "Telescope commands" })
+vim.keymap.set('n', "<leader>ts", pickers.lsp_document_symbols, { desc = "Telescope document symbols" })
+vim.keymap.set('n', "<leader>td", pickers.diagnostics, { desc = "Telescope document symbols" })
+vim.keymap.set('n', "<leader>tgs", pickers.git_status, { desc = "Telescope git status" })
+vim.keymap.set('n', "<leader>tgc", pickers.git_commits, { desc = "Telescope git commits" })
+
+-- Completion
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+local cmp = require("cmp")
+local lspkind = require('lspkind')
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  view = { entries = "custom" },
+  mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      --{ name = 'vsnip' }, -- For vsnip users.
+    }),
+  formatting = { format = lspkind.cmp_format() }
+}
+
+-- Neoformatter
 vim.api.nvim_exec([[
 	augroup fmt
   autocmd!
   autocmd BufWritePre * undojoin | Neoformat
 	augroup END
 ]], false)
-
-vim.g.polyglot_disabled = { "autoindent" }
---vim.cmd("hi CursorLine gui=underline cterm=underline")
---vim.cmd("au InsertEnter * set cul")
---vim.cmd("au InsertLeave * set nocul")
